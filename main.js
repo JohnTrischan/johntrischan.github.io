1f8d2f0c1e8a0793d973354f3ba7eb52310bf637
JohnTrischan <trischan51@gmail.com>
2018-09-18 19:47:26 -0400

    Fixed JS, adjusted logos

--- main.js
+++ main.js
@@ -19,9 +19,28 @@
         document.getElementById("time").innerHTML = h + ":" + "0" + m + " am";
     } else ("Something is horribly wrong")
 
-    var x = document.getElementById("sport");
+   // var x = document.getElementById("sport");
     
    // document.getElementById("sport").innerHTML = x;
+
+
+   var radios = document.getElementsByName('sport');
+   document.addEventListener("click", function() {
+       console.log("In Radio Function")
+for (var i = 0, length = radios.length; i < length; i++)
+{
+ if (radios[i].checked)
+ {
+  // do whatever you want with the checked radio
+  //alert(radios[i].value);
+  //var selection = radios[i].value
+  document.getElementById('radios[i].value')
+  console.log(selection)
+  selection.innerHTML = radios[i].value
+  // only one radio can be logically checked, don't check the rest
+ }
+}
+})
 
     
     
